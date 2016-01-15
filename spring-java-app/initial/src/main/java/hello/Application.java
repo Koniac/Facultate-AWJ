c27e7b5b9e8922fc451f77f0685f91b15c6af140
Koniac <Corax.Albus@gmail.com>
2016-01-15 23:07:41 +0200

    Aplicat modificarile lui Cezar.

--- spring-java-app/initial/src/main/java/hello/Application.java
+++ spring-java-app/initial/src/main/java/hello/Application.java
@@ -1,12 +1,27 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+ @SpringBootApplication
+ public class Application {
+
+     public static void main(String[] args) {
+         SpringApplication.run(Application.class, args);
+     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**/*").allowedOrigins("*");
+            }
+        };
     }
-}
+ }
